0bcbcab91115fef2166819fb5e5e6efdb29cf292
MFMcNolt <141301431+MFMcNolt@users.noreply.github.com>
2023-10-23 14:57:31 +1100

    added questions in the JS file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,66 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "project_title",
+        message: "What is your project tittled?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Briefly describe your project"
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Are there any installations required?"
+    },
+    {
+        type: "input",
+        name: "use",
+        message: "What is the use of the application"
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "Are there any contribution rules?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide test instructions if applicable"
+    },
+    {
+        type: "checkbox",
+        message: "License?",
+        name: "license",
+        choices: [
+            "[MIT License](LICENSE.txt)",
+            "[GNU GPLv3 License](COPYING.txt)",
+        ]
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email account"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your github username"
+    }
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
